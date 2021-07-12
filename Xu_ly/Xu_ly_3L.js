53d93fe4dcdbdf088f0bb2a19d6447265482ad4b
Binh <kimitenla@gmail.com>
2021-07-12 09:29:46 +0700

    ád

--- Xu_ly/Xu_ly_3L.js
+++ Xu_ly/Xu_ly_3L.js
@@ -88,7 +88,6 @@ function update_cuoc_hop(Loai_Doi_tuong, Noi_dung_nhan, db, client) {
         let dk = {
             _id: Doi_tuong.id
         }
-        console.log(id,"day la id")
         let gt = {
             $set: { 
                         status: Doi_tuong.status, 
